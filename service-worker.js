aa7dd120df0fd668c1095ca306a33f29a0fedbc3
polnativenubabu-ai <polnativenubabu@gmail.com>
2026-07-18 11:27:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,46 @@
-const cacheName = "sarvatra-v2";
+const cacheName = "sarvatra-v3";
 
 const filesToCache = [
+  "./",
   "index.html",
   "manifest.json",
   "icon-192.png",
-  "icon-512.png"
+  "icon-512.png",
+  "tools/image-to-pdf.html"
 ];
 
-self.addEventListener("install", function(e){
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache){
-      return cache.addAll(filesToCache);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(e){
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(function(response){
-      return response || fetch(e.request);
-    })
+    fetch(e.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(cacheName).then(cache => {
+          cache.put(e.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
